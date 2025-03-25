4f5f5839ac05da2b30719170add5af19a5039082
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-25 14:29:53 +0100

    Create AccessDecisionEngine.java

---
+++ src/AccessDecisionEngine.java
@@ -0,0 +1,14 @@
+public class AccessDecisionEngine {
+
+    public static String canEnter(boolean hasKeycard, boolean faceRecognized) {
+        // TODO: Use logical operator + ternary
+        // Return "Access Granted" or "Access Denied"
+        return "";
+    }
+
+    // Bonus method with emergency override
+    public static String canEnter(boolean hasKeycard, boolean faceRecognized, boolean isEmergency) {
+        // TODO: If isEmergency → grant access unconditionally
+        return "";
+    }
+}
