1b34c4c58b1a45948f2e5bd49b5ad4c50ecf57e4
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-25 14:30:47 +0100

    Create GuestRestrictions.java

---
+++ src/GuestRestrictions.java
@@ -0,0 +1,10 @@
+public class GuestRestrictions {
+
+    public static String getGuestPermissions(int hoursStayed, boolean isSupervised) {
+        // TODO: Use nested if-else
+        // if hours > 3 and supervised → "Allowed to stay"
+        // if hours > 3 and not supervised → "Must leave soon"
+        // else → "Enjoy your visit!"
+        return "";
+    }
+}
